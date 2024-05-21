1a0e701477d8053e8f876890eff5020c50e6955b
yantingto <99751133+yantingto@users.noreply.github.com>
2024-05-21 22:53:34 +0800

    update_method

--- web_scraper.py
+++ web_scraper.py
@@ -1,28 +1,23 @@
 # Thanks to https://opensource.com/article/21/9/web-scraping-python-beautiful-soup
 import requests
 from bs4 import BeautifulSoup
-from pprint import pprint
 
 url = 'https://cse.hkust.edu.hk/'
 data = requests.get(url)
+max_number = 30
 
-my_data = []
+links = []
 
 html = BeautifulSoup(data.text, 'html.parser')
-articles = html.select('a.post-card')
-max_time = 30
+all_links = html.find_all('a')
 
 i=0
-
-for article in articles:
-
-    title = article.select('.card-title')[0].get_text()
-    excerpt = article.select('.card-text')[0].get_text()
-    pub_date = article.select('.card-footer small')[0].get_text()
-
-    my_data.append({"title": title, "excerpt": excerpt, "pub_date": pub_date})
-    if i >= max_time:
+for link in all_links:
+    href = link.get('href')
+    title = link.get('title') or link.text.strip()
+    links.append({"href": href, "title": title})
+    if i>=max_number:
         break
     i+=1
 
-pprint(my_data)
+print(links)
